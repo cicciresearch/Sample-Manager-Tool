ff9860b1b03b5fe9a5c65ad564621423bdde7687
cicciresearch <47914671+cicciresearch@users.noreply.github.com>
2026-09-16 14:57:35 +0200

    EIS added

---
+++ Migrations/20260916125707_AddEIS.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sample_Manager_Tool.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEIS : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EisMeasurements",
+                columns: table => new
+                {
+                    MeasurementId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PeakFrequencyHz = table.Column<double>(type: "REAL", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EisMeasurements", x => x.MeasurementId);
+                    table.ForeignKey(
+                        name: "FK_EisMeasurements_Measurements_MeasurementId",
+                        column: x => x.MeasurementId,
+                        principalTable: "Measurements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EisMeasurements");
+        }
+    }
+}
